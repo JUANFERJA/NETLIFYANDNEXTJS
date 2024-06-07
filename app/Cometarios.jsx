fb709e449502863d4d8c71f25c52f14071866a38
juanfer <jufer_tar_lch@hotmail.com>
2024-06-07 14:54:38 -0500

    update

--- app/Cometarios.jsx
+++ app/Cometarios.jsx
@@ -21,7 +21,7 @@ const Cometarios = () => {
                         <svg key={indice+"padre6"} className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path key={indice+"padre7"} d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                         </svg>
-                        Members only
+                        Members only *
                     </p>
                     {/* <div className="text-gray-900 font-bold text-xl mb-2">{elemento.titulo}</div> */}
                     <p key={indice+"padre8"} className="text-red-200 text-base  items-center">{elemento.comentario}</p>
