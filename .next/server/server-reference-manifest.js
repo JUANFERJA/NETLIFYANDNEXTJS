ebbb6296599bd02f0fad96c916518c9562fb4236
juanfer <jufer_tar_lch@hotmail.com>
2024-06-07 16:24:15 -0500

    update

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+yvWkwRGeUOmrjjE3k70QrU9+ojzOs0V4FvNego1QUs=\"}"
---
+++ app/prueba/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Prueba = () => {
+  return (
+    <div style={{background:"red"}}>
+      esto es una prueba
+    </div>
+  )
+}
+
+
